8a0d031b8374b6fc208c4bcb6d950321bab8bed0
Mariana Campos <marianacampos@MacBook-Pro-de-actdigital.local>
2021-04-07 22:23:40 -0300

    Desafio 4 - Ignite - SOLID

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,18 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("Your user does not exists!");
+    }
+    if (user.admin === false) {
+      throw new Error("You are not an admin!");
+    }
+
+    const listUsers = this.usersRepository.list();
+
+    return listUsers;
   }
 }
 
